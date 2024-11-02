4326ab7ff4d92b7975ac798863a2ed1b61a5abf8
Lerior <leonardogarcia3500@gmail.com>
2024-11-01 23:48:48 -0600

    try2

--- login.php
+++ login.php
@@ -25,10 +25,10 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
             $result = ["login" => "n", "token" => "Error", "message" => "Usuario no encontrado"];
         }
 
-        header("http/1.1 200 ok");
+        //header("http/1.1 200 ok");
         echo json_encode($result);
     } else {
-        header("HTTP/1.1 400 Bad Request");
+        //header("HTTP/1.1 400 Bad Request");
         echo json_encode(["login" => "n", "token" => "Error", "message" => "Parámetros inválidos"]);
     }
 }
