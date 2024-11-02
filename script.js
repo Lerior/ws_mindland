cd01af06659925d6f303860bfe2afd8645f2efba
Lerior <leonardogarcia3500@gmail.com>
2024-11-01 23:52:47 -0600

    try4

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ function login() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    // Construye la URL con los parámetros de usuario y contraseña
     const url = `https://wsmindland-production.up.railway.app/login.php?user=${encodeURIComponent(username)}&pass=${encodeURIComponent(password)}`;
-    //const url = `http://localhost/mindland/login.php?user=${encodeURIComponent(username)}&pass=${encodeURIComponent(password)}`;
-    
+
+
     fetch(url)
         .then(response => response.json())
         .then(data => {
