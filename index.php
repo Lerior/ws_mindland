3b08bdbdca1056c78a59db83d050ba5b68a47a87
Lerior <leonardogarcia3500@gmail.com>
2024-11-02 20:58:32 -0600

    php+env

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
-    <h2>Login</h2>
-    <h1>Hello, World</h1>
+<?php
+    $host = $_ENV['MYSQLHOST'];
+    $port = $_ENV['MYSQLPORT'];
+    $dbname = $_ENV['MYSQL_DATABASE'];
+    $username = $_ENV['MYSQLUSER'];
+    $password = $_ENV['MYSQLPASSWORD'];
 
-    <script src="script.js"></script>
-</body>
-</html>
+    $con = mysqli_connect($host, $username, $password, $dbname, $port);
