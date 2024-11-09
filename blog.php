08ba77090f7b441eb3aaf83f9a9e970844d2dc76
Lerior <leonardogarcia3500@gmail.com>
2024-11-08 21:00:26 -0600

    blogPOSTfix11

--- blog.php
+++ blog.php
@@ -11,7 +11,7 @@ if(JWT::verify($jwt, Config::SECRET) > 0){
     exit;
 }
 
-$user = JWT::get_data($jwt, Config::SECRET)['data']['user_app'];
+$user = JWT::get_data($jwt, Config::SECRET)['user_app'];
 error_log("Usuario obtenido del token: " . $user);
 if (!$user) {
     header("HTTP/1.1 400 Bad Request");
