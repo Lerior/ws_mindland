194b8359072e7bb638315b422c7811c1be391385
Lerior <leonardogarcia3500@gmail.com>
2024-11-08 18:08:09 -0600

    blogPOSTfix2

--- blog.php
+++ blog.php
@@ -4,19 +4,30 @@ require_once 'jwt.php';
 
 /********BLOQUE DE ACCESO DE SEGURIDAD */
 $headers = apache_request_headers();
-$tmp = $headers['Authorization'];
+$tmp = isset($headers['Authorization']) ? $headers['Authorization'] : '';
 $jwt = str_replace("Bearer ", "", $tmp);
 if(JWT::verify($jwt, Config::SECRET) > 0){
     header("HTTP/1.1 401 Unauthorized");
     exit;
 }
 
-$user = JWT::get_data($jwt, Config::SECRET)['user'];
+$user = JWT::get_data($jwt, Config::SECRET)['user'] ?? null;
+if (!$user) {
+    header("HTTP/1.1 400 Bad Request");
+    echo "No se pudo obtener el usuario del token.";
+    exit;
+}
 $c = conexion();
 $s = $c->prepare("SELECT user_id FROM users WHERE user_app = :user");
 $s->bindValue(":user",$user);
 $s->execute();
 $idUser = $s->fetchColumn();
+
+if (!$idUser) {
+    header("HTTP/1.1 400 Bad Request");
+    echo "Usuario no encontrado.";
+    exit;
+}
 /*** BLOQUE WEB SERVICE REST */
 $metodo = $_SERVER["REQUEST_METHOD"];
 switch($metodo){
